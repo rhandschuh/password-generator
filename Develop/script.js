d092d2818beb547d1dc1013d54574823f415d353
Rachel Handschuh <handschuh.rachel@gmail.com>
2021-05-01 10:51:53 -0400

    updating javascipt, prompts work

--- Develop/script.js
+++ Develop/script.js
@@ -12,62 +12,62 @@ var confirmTheLowerCase;
 var confirmTheUpperCase;
 var confirmTheNumber;
 var confirmTheSpecialCharacters;
+var confirmLength;
 //asking how many characters for password
 function generatePassword() {
   let theLength = (prompt("How many characters for your password? Must be between 8 and 128 characters"))
+  while (confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters");
+    var theeLength = (prompt("How many characters for password?"));
+  }
+  // telling the user the amount of characters that the password will have 
+  
+  alert(`Your password will have ${theLength} characters`);
+  
+  // Determine parameters of password 
+  var confirmTheUpperCase = confirm("confirm if you want numbers in password");
+  var confirmTheLowerCase = confirm("confirm if you want lower case in password");
+  var confirmTheNumber = confirm("confirm if you want numbers in password");
+  var confirmTheSpecialCharacter = confirm("confirm if you want special characters in your password");
+  // a while loop for if answer is outside the parameters 
+  while (confirmTheUpperCase === false && confirmTheLowerCase === false && confirmTheNumber === false && confirmTheSpecialCharacters === false) {
+    alert("must chose at least one criteria");
+    var confirmTheNumber = confirm("confirm if you want numbers in password");
+    var confirmTheSpecialCharacter = confirm("confirm if you want special characters in password");
+    var confirmTheLowerCase = confirm("confirm if you want lower case in password");
+    var confirmTheUpperCase = confirm("confirm if you want upper case in password");
+  }
+  
+  
+  //an empty string to log stuff
+  var theCharacters = []
+  //to merge everything 
+  if (confirmTheNumber) {
+    theCharacters = theCharacters.concat(number)
+  }
+  
+  if (confirmTheSpecialCharacter) {
+    theCharacters = theCharacters.concat(specialCharacters)
+  }
+  
+  if (confirmTheUpperCase) {
+    theCharacters = theCharacters.concat(upperCase)
+  }
+  
+  if (confirmTheLowerCase) {
+    theCharacters = theCharacters.concat(lowerCase)
+  }
+  console.log(theCharacters)
+  for (var i = 0; i < theLength; i++) {
+    aPassword = aPassword + theCharacters[Math.floor(Math.random() * theCharacters.length)];
+    console.log(aPassword)
+    return aPassword;
+  }
 }
 
 function getInput(){
   var userInput = prompt("Please input a number between 8 and 128");
 }
-//loop for if it doesn't work (amount of characters)
-while (confirmLength <= 8 || confirmLength >= 128) {
-  alert("Password length must be between 8-128 characters");
-  var theLength = (prompt("How many characters for password?"));
-}
-// telling the user the amount of characters that the password will have 
-
-alert(`Your password will have ${theLength} characters`);
-
-// Determine parameters of password 
-var confirmTheUpperCase = confirm("confirm if you want numbers in password");
-var confirmTheLowerCase = confirm("confirm if you want lower case in password");
-var confirmTheNumber = confirm("confirm if you want numbers in password");
-var confirmTheSpecialCharacter = confirm("confirm if you want special characters in your password");
-// a while loop for if answer is outside the parameters 
-while (confirmTheUpperCase === false && confirmTheLowerCase === false && confirmTheNumber === false && confirmTheSpecialCharacters === false) {
-  alert("must chose at least one criteria");
-  var confirmTheNumber = confirm("confirm if you want numbers in password");
-  var confirmTheSpecialCharacter = confirm("confirm if you want special characters in password");
-  var confirmTheLowerCase = confirm("confirm if you want lower case in password");
-  var confirmTheUpperCase = confirm("confirm if you want upper case in password");
-}
-
-
-//an empty string to log stuff
-var theCharacters = []
-//to merge everything 
-if (confirmTheNumber) {
-  theCharacters = theCharacters.concat(number)
-}
-
-if (confirmTheSpecialCharacter) {
-  theCharacters = theCharacters.concat(specialCharacters)
-}
-
-if (confirmTheUpperCase) {
-  theCharacters = theCharacters.concat(upperCase)
-}
-
-if (confirmTheLowerCase) {
-  theCharacters = theCharacters.concat(lowerCase)
-}
-console.log(theCharacters)
-for (var i = 0; i < theLength; i++) {
-  aPassword = aPassword + theCharacters[Math.floor(Math.random() * theCharacters.length)];
-  console.log(aPassword)
-}
-return aPassword;
 
 
 // Write password to the #password input
@@ -76,5 +76,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  alert(`Your password will be ${writePassword}`);
 }
+
